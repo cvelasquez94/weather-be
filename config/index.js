6ebb7730e2c95567a764d43d032dce38105fe0ac
Carlos <carlosbrunotbc@gmail.com>
2018-10-21 03:36:54 -0300

    feat(router): new endopoint forecast

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   baseUrl: {
     protocol: 'http',
     hostname: 'api.openweathermap.org',
-    path: '/data/2.5/weather',
+    path: '/data/2.5',
   },
   ApiKey: '7ae289768e9d867301e5d16153ba1888'
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
+'use strict'
+
 const express = require('express');
 const fetch = require('node-fetch');
 const config = require('../config');
 const { hostname, path} = config.baseUrl;
 const app = express.Router();
 
-const generateUrl = (city) => `https://${hostname}${path}?q=${city}&units=metric&appid=${config.ApiKey}`;
-
+const generateUrlWeather = (city) => `https://${hostname}${path}/weather?q=${city}&units=metric&appid=${config.ApiKey}`;
+const generateUrlForecast = (city) => `https://${hostname}${path}/forecast?q=${city}&units=metric&appid=${config.ApiKey}`;
 const currentLocation = async () => {
     const location = await fetch('http://ip-api.com/json/');
     const response = await location.json();
@@ -13,9 +15,14 @@ const currentLocation = async () => {
 }
 
 const currentWeather = async (city) => {
-    console.log(generateUrl(city || 'madrid'));
-    const weather = await fetch(generateUrl(city || 'madrid'));
+    const weather = await fetch(generateUrlWeather(city));
     const response = await weather.json();
+    return response;
+}
+
+const forecastWeek = async (city) => {
+    const forecast = await fetch(generateUrlForecast(city));
+    const response = await forecast.json();
     return response;
 }
 
@@ -35,16 +42,32 @@ app.get('/location', async (req, res, err) => {
 app.get('/current/:city?', async (req, res, err) => {
     try {
         const { city } = req.params;
-        let param;
+        let location;
+        let currentCity;
         if (!city) {
             location = await currentLocation();
-            param = location.city;
+            currentCity = location.city;
         }
-        const weatherToday = await currentWeather(city ? city: param);
+        const weatherToday = await currentWeather(city ? city: currentCity);
         res.send(weatherToday);
     } catch(err) {
         console.log(err)
     }
 })
 
+app.get('/forecast/:city?', async (req, res, err) => {
+    try {
+        const { city } = req.params;
+        let location;
+        let currentCity;
+        if (!city) {
+            location = await currentLocation();
+            currentCity = location.city;
+        }
+        const forecast = await forecastWeek(city ? city : currentCity); 
+        res.status(201).send(forecast);
+    } catch(err) {
+        console.log(err)
+    }
+})
 module.exports = app;
